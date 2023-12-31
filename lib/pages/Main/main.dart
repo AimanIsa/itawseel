7ab2753176853eb7cffb403a63ee743c20989ed5
Muhammad Aiman <m.aimanisa@live.iium.edu.my>
2023-12-31 22:26:33 +0800

    #updated

--- lib/pages/Main/main.dart
+++ lib/pages/Main/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:itawseel/pages/Customer/chatscreen.dart';
 import 'package:itawseel/pages/Login&SignUp/auth_page.dart';
 import 'firebase_options.dart';
 
