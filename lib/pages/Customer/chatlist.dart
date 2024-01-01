429c80efcadfef6c2fdf7cf60e8492cc4ee51423
afiqka <103496058+afiqka@users.noreply.github.com>
2024-01-01 15:42:48 +0800

    #update chatpage and chatscreen

--- lib/pages/Customer/chatlist.dart
+++ lib/pages/Customer/chatlist.dart
@@ -1,15 +1,108 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:itawseel/pages/Customer/chatscreen.dart';
 
-class ChatUserList extends StatefulWidget {
-  const ChatUserList({super.key});
+import 'package:itawseel/themes/colors.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({Key? key}) : super(key: key);
 
   @override
-  State<ChatUserList> createState() => _ChatUserListState();
+  _ChatPageState createState() => _ChatPageState();
 }
 
-class _ChatUserListState extends State<ChatUserList> {
+class _ChatPageState extends State<ChatPage> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  late String currentUserId;
+  late String chatId;
+  late String recipientId;
+
+  @override
+  void initState() {
+    super.initState();
+    currentUserId = _auth.currentUser!.email!;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          const SizedBox(height: 30),
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: _firestore.collection('Users').snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      final user = snapshot.data!.docs[index];
+                      final userId = user.id;
+                      if (userId != currentUserId) {
+                        return Container(
+                          margin: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 10,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(13),
+                            border: Border.all(
+                              color: primaryColor,
+                            ),
+                          ),
+                          child: ListTile(
+                            title: Text(
+                              user['username'],
+                              style: TextStyle(
+                                color: primaryColor,
+                              ),
+                            ),
+                            onTap: () async {
+                              chatId = getChatId(currentUserId, userId);
+                              recipientId = userId;
+                              await _firestore
+                                  .collection('chats')
+                                  .doc(chatId)
+                                  .set({
+                                'users': [currentUserId, userId]
+                              });
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => ChatScreen(
+                                    chatId: chatId,
+                                    recipientId: recipientId,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        );
+                      } else {
+                        return const SizedBox.shrink(); // Hide current user
+                      }
+                    },
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ),
+          const Divider(height: 1),
+        ],
+      ),
+    );
+  }
+
+  String getChatId(String user1, String user2) {
+    if (user1.compareTo(user2) < 0) {
+      return '$user1-$user2';
+    } else {
+      return '$user2-$user1';
+    }
   }
 }
--- lib/pages/Customer/chatscreen.dart
+++ lib/pages/Customer/chatscreen.dart
@@ -1,113 +1,137 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
-  final String currentUserId;
+  final String chatId;
+  final String recipientId;
 
-  final Map<String, dynamic> user; // Received user data from previous screen
-
-  const ChatScreen({Key? key, required this.user, required this.currentUserId})
-      : super(key: key);
-
-  get otherUser => null;
+  const ChatScreen({
+    required this.chatId,
+    required this.recipientId,
+    Key? key,
+  }) : super(key: key);
 
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  final _textController = TextEditingController();
-  late CollectionReference _chatCollection;
-  late Stream<QuerySnapshot> _chatStream;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  late String currentUserId;
+
+  late TextEditingController _messageController;
+  Future<String> getRecipientUsername() async {
+    final doc =
+        await _firestore.collection('Users').doc(widget.recipientId).get();
+    return doc['username'];
+  }
 
   @override
   void initState() {
     super.initState();
-    // In the ChatScreen's initState or build method:
-
-    // Create a unique chat ID based on both user IDs (ordered alphabetically)
-    final orderedUserIds = [
-      widget.currentUserId,
-      widget.otherUser?['id'] ??
-          'defaultUserId', // Provide a default value if null
-    ];
-    final chatId = '${orderedUserIds[0]}_${orderedUserIds[1]}';
-    _chatCollection = FirebaseFirestore.instance
-        .collection('chats')
-        .doc(chatId)
-        .collection('messages');
-
-    // Fetch chat messages on initialization
-    _chatStream = _chatCollection.snapshots();
+    currentUserId = _auth.currentUser!.email!;
+    _messageController = TextEditingController();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.user['username'] != null
-            ? widget.user['username']
-            : 'No username'),
+        title: FutureBuilder<String>(
+          future: getRecipientUsername(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data!);
+            } else if (snapshot.hasError) {
+              return const Text('Error loading username');
+            }
+            return const Text('Loading...');
+          },
+        ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: StreamBuilder<QuerySnapshot>(
-              stream: _chatStream,
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                }
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _firestore
+            .collection('chats')
+            .doc(widget.chatId)
+            .collection('messages')
+            .orderBy('timestamp', descending: false)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                final message = snapshot.data!.docs[index];
+                final messageText = message['text'];
+                final messageSender = message['sender'];
 
-                if (!snapshot.hasData) {
-                  return Center(child: CircularProgressIndicator());
-                }
-
-                final messages =
-                    snapshot.data!.docs.map((doc) => doc.data()).toList();
-                return ListView.builder(
-                  itemCount: messages.length,
-                  itemBuilder: (context, index) {
-                    final message = messages[index];
-                    // Display each message (customize as needed)
-                    return Text(
-                        (message as Map<String, dynamic>)['text']?.toString() ??
-                            "No message text");
-                  },
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: messageSender == currentUserId
+                        ? MainAxisAlignment.end
+                        : MainAxisAlignment.start,
+                    children: [
+                      if (messageSender != currentUserId)
+                        const CircleAvatar(
+                          backgroundImage: AssetImage(
+                              'lib/images/Defaultprofile.jpg'), // Replace with a placeholder image
+                        ),
+                      const SizedBox(width: 10),
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: messageSender == currentUserId
+                              ? Colors.blue[100]
+                              : Colors.grey[200],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Text(messageText),
+                      ),
+                    ],
+                  ),
                 );
               },
-            ),
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: _textController,
-                  decoration: InputDecoration(
-                    hintText: 'Type a message',
-                  ),
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      bottomSheet: Container(
+        padding: const EdgeInsets.all(8),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: _messageController,
+                decoration: const InputDecoration(
+                  hintText: 'Type a message',
                 ),
               ),
-              IconButton(
-                onPressed: () {
-                  _sendMessage();
-                },
-                icon: Icon(Icons.send),
-              ),
-            ],
-          ),
-        ],
+            ),
+            IconButton(
+              onPressed: () async {
+                if (_messageController.text.isNotEmpty) {
+                  await _firestore
+                      .collection('chats')
+                      .doc(widget.chatId)
+                      .collection('messages')
+                      .add({
+                    'sender': currentUserId,
+                    'text': _messageController.text,
+                    'timestamp': FieldValue.serverTimestamp(),
+                  });
+                  _messageController.clear();
+                }
+              },
+              icon: const Icon(Icons.send),
+            ),
+          ],
+        ),
       ),
     );
-  }
-
-  void _sendMessage() {
-    final message = {
-      'text': _textController.text,
-      'sender': widget.currentUserId,
-      'timestamp': DateTime.now().millisecondsSinceEpoch,
-    };
-    _chatCollection.add(message);
-    _textController.clear();
   }
 }
